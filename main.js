891e17faa7577627e3660d694db7bfb0309d2f73
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-03-15 15:05:12 +0000

    ajouter les premier fonctionaliter de mon code js  telque clear display et afficher les button lorsque click

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+let result = document.querySelector(".resultat");
+let buttons = document.querySelectorAll(".button");
 
+buttons.forEach(button =>{
+    button.addEventListener("click", function(){
+        ajouterNumber(this.dataset.number);
+    })
+})
+
+
+function clear(){
+    result.textContent="";
+}
+
+function ajouterNumber(number){
+    result.innerHTML += number;
+}
+
+clear();
